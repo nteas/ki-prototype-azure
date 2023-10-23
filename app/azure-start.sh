d756e30d027324c1c2298f35dd00073e10c94ccc
Ola Årbogen <ola.arbogen@nte.no>
2023-10-23 11:23:53 +0200

    remove azd from start script

--- app/azure-start.sh
+++ app/azure-start.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-echo ""
-echo "Loading azd .env file from current environment"
-echo ""
-
-while IFS='=' read -r key value; do
-    value=$(echo "$value" | sed 's/^"//' | sed 's/"$//')
-    export "$key=$value"
-done <<EOF
-$(azd env get-values)
-EOF
-
-if [ $? -ne 0 ]; then
-    echo "Failed to load environment variables from azd environment"
-    exit $?
-fi
-
 echo 'Creating python virtual environment "app/backend/backend_env"'
 python3 -m venv app/backend/backend_env
 
