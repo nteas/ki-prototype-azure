045df367fe36861226bcf1a7abfab50525298469
Ola Årbogen <ola.arbogen@nte.no>
2023-12-18 15:50:18 +0100

    fix chrome loading in container

--- app/backend/core/utilities.py
+++ app/backend/core/utilities.py
@@ -413,7 +413,9 @@ def get_filename_from_url(url):
 async def scrape_url(url):
     try:
         options = webdriver.ChromeOptions()
+        options.add_argument("--no-sandbox")
         options.add_argument("--headless")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
         driver.get(url)
 
